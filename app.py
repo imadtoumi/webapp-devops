f58c92865e1521c62ce00f4ef36d9cb81dc73741
Imad <imadtoumi8@gmail.com>
2024-04-28 10:58:51 +0000

    Initial install / 1st push

---
+++ app.py
@@ -0,0 +1,24 @@
+# app.py
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+tasks = {}
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add_task', methods=['POST'])
+def add_task():
+    task_name = request.form['name']
+    task_time = request.form['time']
+    task_description = request.form['description']
+    if task_name == "" or task_time == "" or task_description == "":
+        return redirect(url_for('index'))
+    else:
+        tasks[task_name] = {'time': task_time, 'description': task_description}
+        return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
